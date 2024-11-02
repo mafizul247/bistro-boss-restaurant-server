2e874385ebea5ba738f7ae46e037848719099a50
mafizul247 <mafizul247@gmail.com>
2024-11-02 20:00:01 +0600

    Bistro Boss- Mailgun Send Email

--- index.js
+++ index.js
@@ -5,10 +5,55 @@ const dotenv = require('dotenv').config();
 const jwt = require('jsonwebtoken');
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const stripe = require("stripe")(process.env.PAYMENT_SERECT_KEY);
+const nodemailer = require("nodemailer");
+const mg = require('nodemailer-mailgun-transport');
 const port = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json());
+
+/* let transporter = nodemailer.createTransport({
+  host: 'smtp.sendgrid.net',
+  port: 587,
+  auth: {
+    user: "apikey",
+    pass: process.env.SENDGRID_API_KEY
+  }
+}) */
+
+const auth = {
+  auth: {
+    api_key: process.env.EMAIL_API_KEY,
+    domain: process.env.EMAIL_DOMAIN
+  }
+}
+
+const transporter = nodemailer.createTransport(mg(auth));
+
+// Send Payment Confirmation Email
+const sendPaymentConfirmationEmail = (payment) => {
+  transporter.sendMail({
+    from: "mafizul1912@gmail.com", // verified sender email
+    to: payment?.email, // recipient email
+    subject: "Your Order Is Confirm", // Subject line
+    text: "Bistro Boss Restaurant", // plain text body
+    html: `
+      <div>
+        <h2>Payment Confirm</h2>
+        <p>Your Transaction ID is ${payment?.transactionId}</>
+        <p>Total Pay: ${payment?.price}</p>
+      </div>
+    `, // html body
+  }, function (error, info) {
+    if (error) {
+      console.log(error);
+    } else {
+      console.log('Email sent: ' + info.response);
+    }
+  });
+}
+
+
 
 const verifyJWT = (req, res, next) => {
   const authorization = req.headers.authorization;
@@ -197,6 +242,9 @@ async function run() {
       const insertResult = await paymentCollections.insertOne(payment);
       const query = { _id: { $in: payment.cartItemId?.map(id => new ObjectId(id)) } };
       const deleteResult = await cartCollections.deleteMany(query);
+
+      // Send Email
+      sendPaymentConfirmationEmail(payment);
 
       res.send({ insertResult, deleteResult });
     })
